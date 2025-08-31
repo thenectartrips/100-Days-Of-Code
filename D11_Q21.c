fb760a8f533e898e82fad77793d230f701d7d7a0
thenectartrips <prashambik2004@gmail.com>
2025-08-31 23:49:14 +0530

    Initial Upload

---
+++ D11_Q21.c
@@ -0,0 +1,66 @@
+// Q21: Write a program to display the month name and number of days using switch-case for a given month number.
+
+/*
+Sample Test Cases:
+Input 1:
+2
+Output 1:
+February, 28 days
+
+Input 2:
+12
+Output 2:
+December, 31 days
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int month;
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month);
+    switch (month)
+    {
+    case 1:
+        printf("January with 31 days\n");
+        break;
+    case 2:
+        printf("February with 28 days\n");
+        break;
+    case 3:
+        printf("March with 31 days\n");
+        break;
+    case 4:
+        printf("April with 30 days\n");
+        break;
+    case 5:
+        printf("May with 31 days\n");
+        break;
+    case 6:
+        printf("June with 30 days\n");
+        break;
+    case 7:
+        printf("July with 31 days\n");
+        break;
+    case 8:
+        printf("August with 31 days\n");
+        break;
+    case 9:
+        printf("September with 30 days\n");
+        break;
+    case 10:
+        printf("October with 31 days\n");
+        break;
+    case 11:
+        printf("November with 30 days\n");
+        break;
+    case 12:
+        printf("December with 31 days\n");
+        break;
+    default:
+        printf("Invalid month number. Please enter a number between 1 and 12.\n");
+    }
+    return 0;
+}
---
+++ D11_Q22.c
@@ -0,0 +1,48 @@
+// Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    float cost_price, selling_price;
+    printf("Enter Cost Price and Selling Price: ");
+    scanf("%f %f", &cost_price, &selling_price);
+
+    if (selling_price > cost_price)
+    {
+        float profit = selling_price - cost_price;
+        float profit_percentage = (profit / cost_price) * 100;
+        printf("Profit = %.2f%%\n", profit_percentage);
+    }
+    else if (cost_price > selling_price)
+    {
+        float loss = cost_price - selling_price;
+        float loss_percentage = (loss / cost_price) * 100;
+        printf("Loss = %.2f%%\n", loss_percentage);
+    }
+    else
+    {
+        printf("None between Profit and Loss \n");
+    }
+
+    return 0;
+}
