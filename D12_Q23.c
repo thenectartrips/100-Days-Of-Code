a13337115c3e2435924ef875b287e8cb5969aaf1
thenectartrips <prashambik2004@gmail.com>
2025-09-01 20:50:35 +0530

    Initial Upload

---
+++ D12_Q23.c
@@ -0,0 +1,64 @@
+// Q23: Write a program to calculate a library fine based on late days.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+Output 1:
+Fine ₹8
+
+Input 2:
+8
+Output 2:
+Fine ₹32
+
+Input 3:
+15
+Output 3:
+Fine ₹90
+
+Input 4:
+31
+Output 4:
+Membership Cancelled
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int days;
+    printf("Enter the number of days late: ");
+    scanf("%d", &days);
+    if (days <= 5)
+    {
+        printf("Fine ₹%d\n", days * 2);
+    }
+    else if (days <= 10)
+    {
+        printf("Fine ₹%d\n", days * 4);
+    }
+    else if (days <= 15)
+    {
+        printf("Fine ₹%d\n", days * 6);
+    }
+    else if (days <= 20)
+    {
+        printf("Fine ₹%d\n", days * 7);
+    }
+    else if (days <= 25)
+    {
+        printf("Fine ₹%d\n", days * 8);
+    }
+    else if (days <= 30)
+    {
+        printf("Membership Cancelled\n");
+    }
+    else
+    {
+        printf("Entry Restricted\n");
+    }
+
+    return 0;
+}
---
+++ D12_Q24.c
@@ -0,0 +1,39 @@
+//Q24: Write a program to calculate an electricity bill based on units consumed.
+
+/*
+Sample Test Cases:
+Input 1:
+50
+Output 1:
+Bill: ₹250
+
+Input 2:
+150
+Output 2:
+Bill: ₹950
+
+Input 3:
+250
+Output 3:
+Bill: ₹2200
+
+*/
+
+#include <stdio.h>
+
+int main(){
+    int units;
+    printf("Enter the number of units consumed: ");
+    scanf("%d", &units);
+    if (units <= 100){
+        printf("Bill: ₹%d\n", units * 5);
+    }
+    else if (units <= 200){
+        printf("Bill: ₹%d\n", (100 * 5) + (units - 100) * 8);
+    }
+    else{
+        printf("Bill: ₹%d\n", (100 * 5) + (100 * 8) + (units - 200) * 10);
+    }
+
+    return 0;
+}
