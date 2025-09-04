c53d58bffffa30400fbdad87f0c8185223bcd790
thenectartrips <prashambik2004@gmail.com>
2025-09-04 23:31:23 +0530

    Initial Upload

---
+++ D15_Q30.c
@@ -0,0 +1,29 @@
+//Q30: Write a program to reverse a given number.
+
+/*
+Sample Test Cases:
+Input 1:
+1234
+Output 1:
+4321
+
+Input 2:
+100
+Output 2:
+1
+
+*/
+
+#include <stdio.h>
+int main(){
+    int n, reverse_number = 0,i; 
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n!= 0){
+        i = n % 10;          
+        reverse_number= reverse_number * 10 + i;    
+        n = n / 10;         
+    }
+    printf("Reversed number = %d\n", reverse_number);
+    return 0;
+}
