45f863b4f5c0f344549fadf164e303033caf6054
thenectartrips <prashambik2004@gmail.com>
2025-09-08 09:44:02 +0530

    Initial Upload

---
+++ D19_Q37.c
@@ -0,0 +1,35 @@
+/* (Loops without Arrays/Strings)
+Write a program to find the LCM of two numbers.
+
+Input 1:
+4 5
+Output 1:
+20
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, maximum, lcm;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &num1, &num2);
+    maximum = (num1 > num2) ? num1 : num2;
+    while (1)
+    {
+        if (maximum % num1 == 0 && maximum % num2 == 0)
+        {
+            lcm = maximum;
+            break;
+        }
+        ++maximum;
+    }
+
+    printf("LCM of %d and %d is: %d", num1, num2, lcm);
+    return 0;
+}
---
+++ D19_Q38.c
@@ -0,0 +1,32 @@
+/*Q38: Write a program to find the sum of digits of a number.
+
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0, last;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0)
+    {
+        last = num % 10;
+        sum += last;
+        num /= 10;
+    }
+    printf("Sum of digits: %d\n", sum);
+    return 0;
+}
