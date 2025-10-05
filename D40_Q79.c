9b3f0cc7d77591ccfb7cdbbc5fc0619974497273
thenectartrips <prashambik2004@gmail.com>
2025-10-05 22:40:06 +0530

    Initial Upload

---
+++ D40_Q79.c
@@ -0,0 +1,60 @@
+// Q79: Perform diagonal traversal of a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Output 1:
+1 2 4 7 5 3 6 8 9
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+    printf("Enter elements of matrix:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nDiagonal traversal of the matrix:\n");
+
+    for (int k = 0; k < n; k++)
+    {
+        int i = 0, j = k;
+        while (i < n && j >= 0)
+        {
+            printf("%d ", a[i][j]);
+            i++;
+            j--;
+        }
+        printf("\n");
+    }
+
+    for (int k = 1; k < n; k++)
+    {
+        int i = k, j = n - 1;
+        while (i < n && j >= 0)
+        {
+            printf("%d ", a[i][j]);
+            i++;
+            j--;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ D40_Q80.c
@@ -0,0 +1,87 @@
+//Q80 : Multiply two matrices.
+
+/*
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+3 2
+7 8
+9 10
+11 12
+Output 1:
+58 64
+139 154
+
+*/
+
+#include <stdio.h>
+
+        int main()
+{
+    int r1, c1, r2, c2;
+
+    printf("Enter rows and columns of first matrix: ");
+    scanf("%d %d", &r1, &c1);
+
+    printf("Enter rows and columns of second matrix: ");
+    scanf("%d %d", &r2, &c2);
+
+    if (c1 != r2)
+    {
+        printf("Matrix multiplication not possible!\n");
+        return 0;
+    }
+
+    int a[r1][c1], b[r2][c2], res[r1][c2];
+
+    printf("\nEnter elements of first matrix:\n");
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("\nEnter elements of second matrix:\n");
+    for (int i = 0; i < r2; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            res[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            for (int k = 0; k < c1; k++)
+            {
+                res[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    printf("\nResultant matrix after multiplication:\n");
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c2; j++)
+        {
+            printf("%d ", res[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
