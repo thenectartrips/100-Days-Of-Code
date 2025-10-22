f8fbbfe77c0263dd37f93c2f68acc6a03fea3410
thenectartrips <prashambik2004@gmail.com>
2025-10-22 08:36:54 +0530

    Update D13_Q26.c

--- D13_Q26.c
+++ D13_Q26.c
@@ -20,7 +20,7 @@ int main()
 {
     int n, i;
 
-    printf("Enter the value of n: ");
+    printf("Enter the value of n: ") ;
     scanf("%d", &n);
 
     for (i = 1; i <= n; i++)
@@ -30,3 +30,4 @@ int main()
 
     return 0;
 }
+
