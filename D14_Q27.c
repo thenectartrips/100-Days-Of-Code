0906cb7bae651e34bbfda8b0dd334ebd6e7c285b
thenectartrips <prashambik2004@gmail.com>
2025-10-31 07:38:09 +0530

    Update D14_Q27.c

--- D14_Q27.c
+++ D14_Q27.c
@@ -21,10 +21,11 @@ int main(){
     printf("Enter the value of n : ") ;
     scanf("%d", &n);
     for(int i=1; i<=n; i++){
-        sum += (2*i - 1);
+        sum += (2*i - 1) ;
     }
     printf("The sum of first %d odd numbers is : %d\n", n, sum) ;
     return 0;
 }
+
 
 
