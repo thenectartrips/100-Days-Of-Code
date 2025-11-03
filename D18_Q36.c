c9acb7c712c2a6de8694cb128a0040b89b0a6d78
thenectartrips <prashambik2004@gmail.com>
2025-11-03 08:11:54 +0530

    Update D18_Q36.c

--- D18_Q36.c
+++ D18_Q36.c
@@ -18,7 +18,7 @@ Output 2:
 
 int main(){
     int num1, num2, i, hcf;
-    printf("Enter two positive integers: ");
+    printf("Enter two positive integers: ") ;
     scanf("%d %d" , &num1, &num2);
     for(i = 1; i <= num1 && i <= num2; ++i){
         if(num1 % i == 0 && num2 % i == 0){
@@ -27,4 +27,5 @@ int main(){
     }
     printf("HCF of %d and %d is: %d", num1, num2, hcf);
     return 0;
+
 }
