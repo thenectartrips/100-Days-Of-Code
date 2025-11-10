146883bd8e49398ab9ce8644c4e68df751d6d371
thenectartrips <prashambik2004@gmail.com>
2025-11-11 01:21:05 +0530

    Update D7_Q14.c

--- D7_Q14.c
+++ D7_Q14.c
@@ -24,8 +24,9 @@ int main()
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
         printf("Given character is vowel\n");
     else if (ch == 'b' || ch == 'c' || ch == 'd' || ch == 'f' || ch == 'g' || ch == 'h' || ch == 'j' || ch == 'k' || ch == 'l' || ch == 'm' || ch == 'n' || ch == 'p' || ch == 'q' || ch == 'r' || ch == 's' || ch == 't' || ch == 'v' || ch == 'w' || ch == 'x' || ch == 'y' || ch == 'z')
-        printf("Given character is consonant\n");
+        printf("Given character is consonant\n") ;
     else
         printf("Invalid input...Please enter a character\n");
     return 0;
+
 }
